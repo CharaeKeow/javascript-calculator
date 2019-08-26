37d7bcb374f5828fac42da877250950ac52925d3
charae <charaekeow11@gmail.com>
2019-08-27 02:31:55 +0800

    Updates

---
+++ precache-manifest.ee7e7d6586c90e5d13832c1d9bc116ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819986a72f79d2fe810356cec60fc7d5",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "564aceb6c4c0ace052c1",
+    "url": "/javascript-calculator/static/css/main.1dcba474.chunk.css"
+  },
+  {
+    "revision": "fd0129913273e15b0e5e",
+    "url": "/javascript-calculator/static/js/2.b403ceb5.chunk.js"
+  },
+  {
+    "revision": "564aceb6c4c0ace052c1",
+    "url": "/javascript-calculator/static/js/main.ca81970b.chunk.js"
+  },
+  {
+    "revision": "af3dc86e0aee640c7c09",
+    "url": "/javascript-calculator/static/js/runtime~main.b6bacda3.js"
+  }
+]);
